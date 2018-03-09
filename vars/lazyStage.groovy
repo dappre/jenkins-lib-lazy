05a358aa9f5b36f5f74abe188fa1732645fe25ba
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-03-09 15:22:44 +0100

    Use config and prepareShTasks in lazyStage
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/lazyStage.groovy
+++ vars/lazyStage.groovy
@@ -3,6 +3,7 @@
 def call (body) {
 	def params = [
 		name:		null,
+		config:		null,
 		tasks:		[],
 		dockerArgs:	'',
 	]
@@ -21,37 +22,49 @@ def call (body) {
 		error err
 	}
 
-	params.tasks.each { task ->
-		// Prepare dists to be used for this task bloc
-		def dists = []
-		if (task.on == '*') {
-			// TODO: populate from config 
-			dists = [ 'all', 'dist', 'available', ]
-		} else {
-			dists += task.on
-		}
-
-		// Collect steps to be executed
-		def steps = []
-		if (task.exec instanceof Closure) {
-			steps += task.exec
-		} else if (task.exec instanceof List) {
-			task.exec.each { subtask ->
-				steps += { sh name + '/' + subtask.exec }
+	stage(Character.toUpperCase(params.name.charAt(0)).toString() + params.name.substring(1)) {
+		params.tasks.each { task ->
+			// Prepare dists to be used for this task bloc
+			def dists = []
+			if (task.on == '*') {
+				// TODO: populate from config 
+				dists = [ 'all', 'dist', 'available', ]
+			} else {
+				dists += task.on
 			}
-		} else if (task.exec instanceof String) {
-			steps += { sh params.name + '/' + task.exec }
-		} else {
-			error "No idea what to do with ${task.exec}"
-		}
+	
+			// Execute steps inside or outside dists
+			dists.each { dist ->
+				node('master') {
+					// Collect steps to be executed
+					def steps = []
+					if (task.exec instanceof Closure) {
+						// If task is a Closuer, just add it in the step list
+						steps += task.exec
+					} else {
+						// Prepare shell scripts from list or string
+						def shTasks = null
+						if (task.exec instanceof List) {
+							shTasks = prepareShTasks(params.name, params.config, dist, task.exec)
+						} else if (task.exec instanceof String) {
+							shTasks = prepareShTasks(params.name, params.config, dist, [ task.exec ])
+						} else {
+							// Give up if not a Closure, not a List and not a String!
+							error "No idea what to do with ${task.exec}"
+						}
+						// Collect all shel steps
+						shTasks.each { shTask ->
+							steps += { sh shTask }
+						}
+					}
 
-		// Execute steps inside or outside dists
-		dists.each { dist ->
-			echo "Need to exec the following steps inside (${dist})"
-			steps.each { step ->
-				step()
+					echo "Need to exec the following steps inside (${dist})"
+					steps.each { step ->
+						step()
+					}
+					echo "Done"
+				}
 			}
-			echo "Done"
 		}
 	}
 }
